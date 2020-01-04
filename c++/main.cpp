8cf70c6a7f4281cd0dbe9b8fb47f9ebab5ed139b
tsengyushiang <tsengyushiang@gmail.com>
2020-01-04 11:24:14 +0800

    add c++ codeing style

---
+++ c++/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// if you use vscode, set task.json and  press 'ctrl+shift+b' to run code
+
+int main()
+{
+  cout << "Hello world" << endl;
+  int a = 0;
+  return 0;
+}
