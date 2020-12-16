6c22a986d35e914274de8af7b0c2427cb2aa8bc7
Aaron <tsengyushiang@gamil.com>
2020-12-16 20:57:22 +0800

    feat : gitback depend on latest update

--- github/backup.sh
+++ github/backup.sh
@@ -1,2 +1,3 @@
-GHUSER=tsengyushiang; 
-curl -H 'Authorization: token 1bb635a629d7d803c02af48188df34b710ec0786' -s "https://api.github.com/search/repositories?q=user:$GHUSER+is:private" | grep -w clone_url | grep -o '[^"]\+://.\+.git' | xargs -L1 git clone
+GHUSER=tsengyushiang;
+LATESTBACKUPDATE=2020-10-28;
+curl -H 'Authorization: token 1bb635a629d7d803c02af48188df34b710ec0786' -s "https://api.github.com/search/repositories?q=user:$GHUSER+is:private+is:private+pushed:>$LATESTBACKUPDATE" | grep -w clone_url | grep -o '[^"]\+://.\+.git' | xargs -L1 git clone
