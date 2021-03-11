7c843c8b1735f8d2bcfa53fc066c51e9810c3e01
yushiang <tsengyushiang@gmail.com>
2021-03-11 16:57:00 +0800

    git backup

--- github/backup.sh
+++ github/backup.sh
@@ -1,4 +1,4 @@
 GHUSER=tsengyushiang;
-LATESTBACKUPDATE=2020-10-28;
+LATESTBACKUPDATE=2021-03-11;
 curl -H 'Authorization: token 1bb635a629d7d803c02af48188df34b710ec0786' -s "https://api.github.com/search/repositories?q=user:$GHUSER+is:private+pushed:>$LATESTBACKUPDATE" | grep -w clone_url | grep -o '[^"]\+://.\+.git' | xargs -L1 git clone
 curl -H 'Authorization: token 1bb635a629d7d803c02af48188df34b710ec0786' -s "https://api.github.com/search/repositories?q=user:$GHUSER+pushed:>$LATESTBACKUPDATE" | grep -w clone_url | grep -o '[^"]\+://.\+.git' | xargs -L1 git clone
